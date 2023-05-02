a5bea69b51f02e014220fbe4adcbb88360c982b3
oshadaera68 <mindartlk010@gmail.com>
2023-05-02 20:55:23 +0530

    updated

--- Singleton/src/Demo.java
+++ Singleton/src/Demo.java
@@ -1,7 +1,7 @@
 public class Demo {
     public static void main(String[] args) {
-        Singleton singleton1 = new Singleton();
-        Singleton singleton2 = new Singleton();
+        Singleton singleton1 = Singleton.getInstance();
+        Singleton singleton2 = Singleton.getInstance();
         System.out.println(singleton1==singleton2);
     }
 }
